1080578f5524e8f139525f812d6074683b625f39
Ali <abdulmuqsit10101@gmail.com>
2020-05-27 18:00:12 +0500

    [stuff.js]: 2nd way of module.exports

--- stuff.js
+++ stuff.js
@@ -1,13 +1,9 @@
-const counter = function (arr) {
+module.exports.counter = function (arr) {
   console.log('the number of elements in array is : ', arr.length);
 };
 
-const adder = function (num1, num2) {
+module.exports.adder = function (num1, num2) {
   console.log('the sum of two numbers is : ', num1 + num2);
 };
 
-const pi = 3.1416;
-
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+module.exports.pi = 3.1416;
