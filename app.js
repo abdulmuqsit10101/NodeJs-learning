685bf6701ca5cd6c575605dca647f0f1f30f5d9a
Ali <abdulmuqsit10101@gmail.com>
2020-05-28 09:35:34 +0500

    Node Event Emitter ( Part 2 ) with more details.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const events = require('events');
+const util = require('util');
 
-const myEmitter = new events.EventEmitter();
+const Person = function (name) {
+  this.name = name;
+};
 
-myEmitter.on('someEvent', function (msg) {
-  console.log('msg : ', msg);
+util.inherits(Person, events.EventEmitter);
+
+const james = new Person('James');
+const mary = new Person('Mary');
+const ryu = new Person('Ryu');
+
+const people = [james, mary, ryu];
+people.forEach(function (person) {
+  person.on('speak', function (msg) {
+    console.log(person.name, ' : ', msg);
+  })
 });
 
-myEmitter.emit('someEvent', 'hello world!');
+james.emit('speak', 'hello I am James and speaker');
+mary.emit('speak', 'hello I am Mary');
+ryu.emit('speak', `hello I am ${ryu.name}`);
