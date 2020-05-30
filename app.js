dea8244448afe4e8eeee4129a75b62e213befc54
Ali <abdulmuqsit10101@gmail.com>
2020-05-30 15:18:41 +0500

    [app.js]: A coding correction for removing file (fs).
    Basically a callback is required in unlink method. So, added that callback in fs.unlink().

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// module to read write files.
+// module to read write files and to create and remove directories ( folders ).
 const fs = require('fs');
 
-// read a file
-fs.readFile('readMe.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  // write a file
-  fs.writeFile('./writeMe.txt', data, function(err){
-    if (err) console.log(err);
-    else console.log('File saved');
-  });
-});
+// // read a file
+// fs.readFile('readMe.txt', 'utf8', function (err, data) {
+//   if (err) throw err;
+//   // write a file
+//   fs.writeFile('./writeMe.txt', data, function (err) {
+//     if (err) console.log(err);
+//     else console.log('File saved');
+//   });
+// });
 
 // removing a file.
-fs.unlink('./writeMe.txt');
+fs.unlink('./writeMe.txt', function(){
+  console.log('file deleted');
+});
