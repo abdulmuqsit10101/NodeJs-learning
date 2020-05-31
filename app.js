3c857e94bdde14e676585d45d0c4fce52e86efcc
Ali <abdulmuqsit10101@gmail.com>
2020-05-31 12:53:46 +0500

    [app.js]: Server Created
    This server would run on 127.0.0.1:3000 or localhost:3000 port on browser.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // module require for creating server
 const http = require('http');
 
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hey Ali');
+});
 
+server.listen(3000, '127.0.0.1');
+console.log('server listening on port 3000');
