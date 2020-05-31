a26609321adbc8013bcf9a408cb7b3acb36c07d9
Ali <abdulmuqsit10101@gmail.com>
2020-05-31 13:08:48 +0500

    [app.js]: Streams and Buffers

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// module require for creating server
-const http = require('http');
+// Streams and Buffers
 
-const server = http.createServer(function (req, res) {
-  console.log('request was made', req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hey Ali');
-});
-
-server.listen(3000, '127.0.0.1');
-console.log('server listening on port 3000');
+// Buffers is temporary storage spot of chunks of data to transfer them from one place to other.
+// And Stream is this flow of data in small chunks for better node server performance. We can create streams in Node Js.
