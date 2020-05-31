d48503b97fa0a77664caeaf0c66d3aeab22c80fb
Ali <abdulmuqsit10101@gmail.com>
2020-05-31 16:45:28 +0500

    Readable and writeable streams.

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 
 // Buffers is temporary storage spot of chunks of data to transfer them from one place to other.
 // And Stream is this flow of data in small chunks for better node server performance. We can create streams in Node Js.
+
+// Read Streams
+
+const fs = require('fs');
+const myReadStream = fs.createReadStream(__dirname + '/dummyText.txt', 'utf8');
+const myWriteStream = fs.createWriteStream(__dirname + '/streamWritten.txt');
+
+myReadStream.on('data', function (chunk) {
+  // Write Streams
+  myWriteStream.write(chunk);
+});
+
