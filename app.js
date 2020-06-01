1247103e2ba39cec1e708514a46494f3b4402f42
Ali <abdulmuqsit10101@gmail.com>
2020-06-02 04:05:29 +0500

    [app.js]: Introduction to express.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// Node's express package installed.
+// Introduction to express
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('This is home page.');
+});
+
+app.get('/contact', function (req, res) {
+  res.send('This is contact page.');
+});
+
+app.listen('3000');
