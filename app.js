262214a0144d15d862ed3ff3a1140a5be0e2d0c0
Ali <abdulmuqsit10101@gmail.com>
2020-06-02 04:16:38 +0500

    [app.js]: express params explained.

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.get('/contact', function (req, res) {
   res.send('This is contact page.');
 });
 
+app.get('/profile/:id', function (req, res) {
+  res.send('You requested to see a profile with id ' + req.params.id);
+});
+
 app.listen('3000');
