7c780933113b3213b82515be2f85606b38794c6f
Ali <abdulmuqsit10101@gmail.com>
2020-06-02 18:51:23 +0500

    Html pages converted to views and partial nav is included on each view's top.

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/home.html');
+  res.render('home');
 });
 
 app.get('/contact', function (req, res) {
-  res.sendFile(__dirname + '/contact.html');
+  res.render('contact');
 });
 
 app.get('/profile/:name', function (req, res) {
